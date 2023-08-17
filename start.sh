ac7f6e10fd9d2a3109deab84e6de1cfd48c9e9b5
matipatapon <matipietka@gmail.com>
2023-08-17 20:27:27 +0200

    Added mpg123 check

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
 echo """
 SMALL SCRIPT THAT INDICATES BATTERY LEVEL CHANGE AFTER REACHING THE THRESHOLD
 
 MADE BY MATEUSZ PIETKA matipietka@gmail.com
 """
+
+mpg123 --version
+
+if [ $? != 0 ]
+then
+	echo "mpg123 not installed !!!"
+	exit 1	
+fi
 
 python -m venv env
 ./env/bin/pip install gtts
